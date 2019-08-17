971a4cc7b0efa90b4eaacd02db6662a3affc9372
zc <zachcaceres@gmail.com>
2019-08-18 00:51:55 +0300

    update outside click hook

--- react/useOutsideClick.js
+++ react/useOutsideClick.js
@@ -3,28 +3,36 @@ import {
 } from 'react'
 import ReactDOM from 'react-dom'
 
-const useOnOutsideClick = (ref, callback, options = {}) => {
+/**
+ * Given a `targetRef` listen for clicks where clicked node is not contained within the `targetRef.current`, then invoke `handler`.
+ *
+ * Optionally `useCapture` via `options`.
+ *
+ * @param {Object} targetRef React ref object
+ * @param {Function} handler function to invoke on outside click
+ * @param {Object} options for extensibility, pass { useCapture } to capture event
+ */
+const useOnOutsideClick = (targetRef, handler, options = {}) => {
   const {
     useCapture
   } = options
 
   useEffect(() => {
-    const handleClick = e => {
+    function handleClick(e) {
 
       /**
-       * Edge case alert! We have to use ReactDOM.findDOMNode to make sure .contains reflects the DOM and not react's virtual dom.
-       *
-       * `findDOMNode` will give us the underly node
+       * Future self alert: We have to use ReactDOM.findDOMNode to make sure .contains reflects the browser's DOM and not React's virtual dom. `findDOMNode` will give us the underlying node.
        */
-      if (!ref.current || ReactDOM.findDOMNode(ref.current).contains(e.target)) return
-      else callback()
+      if (!targetRef.current || ReactDOM.findDOMNode(targetRef.current).contains(e.target)) return
+      else handler(e)
     }
+
     document.addEventListener('click', handleClick, Boolean(useCapture))
 
     return () => {
       document.removeEventListener('click', handleClick, Boolean(useCapture))
     }
-  }, [ref, callback, useCapture])
+  }, [targetRef, handler, useCapture])
 }
 
 export default useOnOutsideClick
