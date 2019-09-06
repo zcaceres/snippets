413280b3d71dd44f8cb25697f5478a3f1c35dd45
zc <zachcaceres@gmail.com>
2019-09-06 15:07:34 +0200

    new hotkey hook

--- react/useHotkey.js
+++ react/useHotkey.js
@@ -1,16 +1,22 @@
+import {
+  useEffect
+} from 'react'
+
 /**
  * Written because `react-hotkeys-hooks` on npm requires that the component have focus to fire hotkeys.
  *
- * Better to have them on the document level if an element is present on the page.
+ * For some cases it's better to have key bindings on the document level simply if an element is present on the page.
  *
  * @param {String} targetKey key to be matched
  * @param {Function} handler function to be called
  * @param {Object} options for extensibility
  */
-export function useHotkey(targetKey, handler, options = {}) {
+export default function useHotkey(targetKey, handler, options = {}) {
   const {
     withCtrlKey,
-    withMetaKey
+    withMetaKey,
+    useCapture,
+    useKeyUpEvent,
   } = options
   useEffect(() => {
     function takeAction(e) {
@@ -25,16 +31,27 @@ export function useHotkey(targetKey, handler, options = {}) {
         // chord with metaKey
       } else if (withMetaKey && (metaKey && key === targetKey)) {
         handler(e)
-        // No chord
-      } else if (!ctrlKey && !metaKey &&
+        // No chord... TODO: make this not so ugly
+      } else if ((!ctrlKey && !metaKey) &&
         (!withMetaKey && !withCtrlKey) &&
         (key === targetKey)) {
         handler(e)
       }
     }
 
-    document.addEventListener('keydown', takeAction)
-    return () => document.removeEventListener('keydown', takeAction)
+    if (useKeyUpEvent) {
+      document.addEventListener('keyup', takeAction, Boolean(useCapture))
+    } else {
+      document.addEventListener('keydown', takeAction, Boolean(useCapture))
+    }
 
-  }, [handler, options, withCtrlKey, targetKey, withMetaKey])
+    return () => {
+      if (useKeyUpEvent) {
+        document.removeEventListener('keyup', takeAction, Boolean(useCapture))
+      } else {
+        document.removeEventListener('keydown', takeAction, Boolean(useCapture))
+      }
+    }
+
+  }, [handler, options, withCtrlKey, targetKey, withMetaKey, useCapture, useKeyUpEvent])
 }
