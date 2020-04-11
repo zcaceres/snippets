9ddc152f153dd968382ba074e61707f44601a422
zc <zachcaceres@gmail.com>
2020-04-11 15:24:42 -0400

    jupyter  style

---
+++ jupyter/nbstyles.py
@@ -0,0 +1,4 @@
+from IPython.display import set_matplotlib_formats
+set_matplotlib_formats('retina')
+from IPython.core.display import display, HTML
+display(HTML("<style>.container { width:100% !important; }</style>"))
